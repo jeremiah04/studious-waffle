3ace09f69baa348cac7575e1751fc2ba0d00ebb0
Jeremiah Mall <jeremiahmall@yahoo.com>
2017-01-25 20:51:11 +0000

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  resources :microposts
   resources :users
+  resources :microposts
+  #For details on the DSL avaiable within This file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 end
